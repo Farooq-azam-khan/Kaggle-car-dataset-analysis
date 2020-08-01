83527301ff516013f14fb7aa08c980fba3cf2421
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-08-01 14:10:34 -0400

    saved

--- renderer.R
+++ renderer.R
@@ -3,7 +3,8 @@ install.packages('tidyverse')
 install.packages("knitr")
 install.packages('DAAG')
 install.packages('bootstrap')
+install.packages("gvlma")
+
 
 library(rmarkdown)
-
 render("used_car_mkdown.Rmd")
