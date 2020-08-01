a311196d017533c85b59cfbf6ad2f6703cdaabb4
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-08-01 14:10:04 -0400

    looked at r webpage for linear analyisis, todo: read more about it and try to use it for the rest of the dataset

--- renderer.R
+++ renderer.R
@@ -1,7 +1,9 @@
 install.packages('ggplot2')
 install.packages('tidyverse')
+install.packages("knitr")
+install.packages('DAAG')
+install.packages('bootstrap')
 
 library(rmarkdown)
-
 
 render("used_car_mkdown.Rmd")
